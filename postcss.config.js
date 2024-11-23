0df6c1966d6f039766d96a5cfc9ea39c635743d8
Paweł Soboń <ppawel1388@gmail.com>
2024-11-23 05:23:33 +0100

    Fixed a problem with nested CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+module.exports = {
+  plugins: [
+    require("postcss-nested"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+  ],
 };
